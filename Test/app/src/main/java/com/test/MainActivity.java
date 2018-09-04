0e80bbc2158ee8043cbd5eb0ac85db0f2d0df956
zsc2018w <zsc971@163.com>
2018-09-04 17:50:21 +0800

    瞎改2

--- Test/app/src/main/java/com/test/MainActivity.java
+++ Test/app/src/main/java/com/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Toast.makeText(this, "你改我也改", Toast.LENGTH_SHORT).show();
 
     }
 }
